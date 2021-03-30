432b653ad61dad0c9961b2af8af30e6e8ee94809
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-30 22:23:04 +0530

    Create Day19.cpp

---
+++ Day19.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+  public:
+    virtual int divisorSum(int n) override {
+        int sum = 0;
+        if ( 1 == n ) { return 1; }
+        
+        // Identify the divisors
+        for (int lo = 1, hi = n ; lo < hi ; ++lo) {
+            if (n % lo) { continue; }
+            sum += lo;
+            hi = (n / lo);
+            if (hi <= lo) { break; }
+            sum += hi;
+        }
+        
+        return sum;
+    }
+};
+int main(){
