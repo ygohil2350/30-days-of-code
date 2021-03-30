b60de56052e0e0d55cff9f635cde98cdde984241
ygohil2350 <69479202+ygohil2350@users.noreply.github.com>
2021-03-30 22:53:36 +0530

    Update and rename Day27.cpp to Day27.java

---
+++ Day27.java
@@ -0,0 +1,36 @@
+
+
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            // complete this function
+             int [] arr = {};
+             return arr;
+        }
+    }
+
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            // complete this function
+            int [] arr = {1, 2};
+            return arr;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            // complete this function
+            int [] arr = {1, 1};
+            return arr;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
+
